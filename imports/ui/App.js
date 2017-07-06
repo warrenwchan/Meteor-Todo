736d43c07144fd1e96292386a4ab1154c231092e
Warren Chan <warrenchan@warren-2.hitronhub.home>
2017-07-06 16:56:44 -0700

    Add input field to insert new tasks into db with ui

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -6,6 +7,19 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
 
 class App extends Component {
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -17,6 +31,14 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Enter new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -33,6 +55,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: {createdAt: -2} }).fetch(),
   };
 }, App);
